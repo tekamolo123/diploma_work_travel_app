0ac8f1f06b32cfd35eeb1001985081095da33ab3
Leonid Raiev <raevoy.ph@gmail.com>
2026-03-19 03:23:21 +0100

    Update code documentation and docs generation

--- generate_docs.bat
+++ generate_docs.bat
@@ -3,6 +3,10 @@ echo Running flutter pub get...
 call flutter pub get
 if errorlevel 1 exit /b 1
 
+echo Running flutter analyze...
+call flutter analyze
+if errorlevel 1 exit /b 1
+
 echo Generating documentation...
 call dart doc
 if errorlevel 1 exit /b 1
